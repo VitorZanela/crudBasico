1dcb2a0ed36e8b95d7c56df242a1fcb89826e63f
vitorzanela <vitor.mardig@gmail.com>
2025-05-09 00:36:58 -0300

    Incluso funções (menu, adcionarContato, verContato)
    Faltando adicionar as funções atualizarContato e removerContato

--- PRINCIPAL.py
+++ PRINCIPAL.py
@@ -1,40 +1,79 @@
 def menu():
-    print  ('[1] Adicionar contato\n'
-            '[2] Ver contatos\n'
-            '[3] Atualizar contato\n'
-            '[4] Remover contato\n'
-            '[0] Sair')
-    while True:
-        msg = int(input('Digite a opção: '))
-        if msg == 1:
-            adicionar()
-        if msg == 2:
-            vercontato()
-        elif msg == 0:
-            break
+    global usuarios
+    print  ('[1] Adicionar novo contato\n'
+        '[2] Ver contatos\n'
+        '[0] Sair')
+    primeiraEscolha = int(input('Digite a opção1: '))
+    if primeiraEscolha == 1:
+        adicionarContato()
+    elif primeiraEscolha == 2:
+        if len(usuarios) == 0:
+            print('Não há contatos cadastrados! Tente novamente')
+            menu()
+        else:
+            verContato()
+    elif primeiraEscolha == 0:
+        print('Programa Encerrado! Volte Sempre.')
 
-def adicionar():
+
+def adicionarContato():
     global cadastro
     global usuarios
     cadastro['Nome'] = str(input("Nome: "))
     cadastro['Telefone'] = int(input("Telefone: "))
     cadastro['Email'] = str(input('Email: '))
     usuarios.append(cadastro.copy())
+    print('USUARIO CADASTRADO COM SUCESSO!!')
+    menu()
 
-    return cadastro
 
-def vercontato():
+def verContato():
     global cadastro
     global usuarios
-    print('Cod', end=' ')
     for p in cadastro.keys():
         print(f'{p:<15}', end='')
     print()
-    for chave, v in enumerate(usuarios):
-        print(f'{chave:<3}', end='')
-        for valores in cadastro.values():
-            print(f'{str(valores):<15}', end=' ')
+    for chave, valores in enumerate(usuarios):
+        for v in valores.values():
+            print(f'{str(v):<14}', end=' ')
         print()
+    while True:
+        print('[1] Adicionar novo contato\n'
+            '[2] Atualizar contato\n'
+            '[3] Remover contato\n'
+            '[0] Sair')
+        msg = int(input('Digite a opção: '))
+        if msg == 1:
+            adicionarContato()
+        elif msg == 2:
+            atualizarContato()
+        elif msg == 3:
+            print()#removerContato()
+        elif msg == 0:
+            print('Programa Encerrado! Volte Sempre.')
+            break
+
+def atualizarContato():
+    global cadastro
+    global usuarios
+    print('cod ', end=' ')
+    for p in cadastro.keys():
+        print(f'{p:<15}', end='')
+    print()
+    for chave, valores in enumerate(usuarios):
+        print(f'{chave:<5}', end='')
+    for v in valores.values():
+        print(f'{str(v):<15}', end='')
+    print()
+    while True:
+        escolha = int(input('Qual contato deseja atualizar? (999 para cancelar): '))
+        if escolha == 999:
+            break
+        elif escolha >= len(usuarios):
+            print('ERRO! Opção invalida')
+        else:
+            print(f' -> ALTERANDO DADOS DE USUÁRIO: {usuarios[escolha]["Nome"]}')
+
 
 
 
@@ -44,6 +83,8 @@ usuarios = []
 #Menu interativo
 menu()
 
+    
 
-
+print(cadastro)
+print(usuarios)
 
