1b2abdcd9a81bd42a2d9e69522708baf86618575
vitorzanela <vitor.mardig@gmail.com>
2025-05-09 01:50:38 -0300

    Ajustado cores e layout do menu de interação com o usuario

--- PRINCIPAL.py
+++ PRINCIPAL.py
@@ -1,57 +1,91 @@
+from time import sleep
+
+
 def menu():
-    global usuarios
-    print  ('[1] Adicionar novo contato\n'
-        '[2] Ver contatos\n'
-        '[0] Sair')
-    primeiraEscolha = int(input('Digite a opção1: '))
-    if primeiraEscolha == 1:
-        adicionarContato()
-    elif primeiraEscolha == 2:
-        if len(usuarios) == 0:
-            print('Não há contatos cadastrados! Tente novamente')
-            menu()
-        else:
-            verContato()
-    elif primeiraEscolha == 0:
-        print('Programa Encerrado! Volte Sempre.')
+    while True:
+        global usuarios
+        sleep(1)
+        print(f'-'*40)
+        print('MENU PRINCIPAL'.center(40))
+        print('-'*40)
+        print  ('\033[1;33m1\033[m - \033[1;34mAdicionar novo contato\033[m\n'
+            '\033[1;33m2\033[m - \033[1;34mVer contatos\033[m\n'
+            '\033[1;33m3\033[m - \033[1;34mSair\033[m')
+        print('-'*40)
+        escolha = int(input('\033[0;33mSua Opção:\033[m '))
+        if escolha == 1:
+            adicionarContato()
+            break
+        elif escolha == 2:
+            if len(usuarios) == 0:
+                print('-'*40)
+                print('\033[0;31mNão há contatos cadastrados! Tente novamente\033[m')
+                menu()
+                break
+            else:
+                verContato()
+                break
+        elif escolha == 3:
+            print('-'*40)
+            print('Programa Encerrado! Volte Sempre.')
+            print('-'*40)
+            break
 
 
 def adicionarContato():
     global cadastro
     global usuarios
-    cadastro['Nome'] = str(input("Nome: "))
+    print(f'-'*40)
+    print('CADASTRO DE USUARIO'.center(40))
+    print('-'*40)
+    cadastro['Nome'] = str(input("Nome:     "))
     cadastro['Telefone'] = int(input("Telefone: "))
-    cadastro['Email'] = str(input('Email: '))
+    cadastro['Email'] = str(input('Email:    '))
     usuarios.append(cadastro.copy())
-    print('USUARIO CADASTRADO COM SUCESSO!!')
+    print('-'*40)
+    print('\033[1;32mUSUARIO CADASTRADO COM SUCESSO!!\033[m')
     menu()
 
 
 def verContato():
     global cadastro
     global usuarios
+    print(f'-'*40)
+    print('USUARIOS'.center(40))
+    print('-'*40)
     for p in cadastro.keys():
-        print(f'{p:<15}', end='')
+        print(f'\033[4m{p:<15}\033[0m', end='')
     print()
     for chave, valores in enumerate(usuarios):
         for v in valores.values():
             print(f'{str(v):<14}', end=' ')
         print()
+    print('-'*40)
     while True:
-        print('[1] Adicionar novo contato\n'
-            '[2] Atualizar contato\n'
-            '[3] Remover contato\n'
-            '[0] Sair')
-        msg = int(input('Digite a opção: '))
+        print  ('\033[1;33m1\033[m - \033[1;34mAdicionar novo contato\033[m\n'
+        '\033[1;33m2\033[m - \033[1;34mAtualizar contato\033[m\n'
+        '\033[1;33m3\033[m - \033[1;34mRemover contato\033[m\n'
+        '\033[1;33m0\033[m - \033[1;34mSair\033[m')
+        print('-'*40)
+        msg = int(input('\033[0;33mSua Opção:\033[m '))
         if msg == 1:
             adicionarContato()
+            break
         elif msg == 2:
             atualizarContato()
+            break
         elif msg == 3:
             print()#removerContato()
-        elif msg == 0:
-            print('Programa Encerrado! Volte Sempre.')
             break
+        elif msg == 0:
+            print('-'*40)
+            print('Programa Encerrado! Volte Sempre.')
+            print('-'*40)
+            break
+        else:
+            print('\033[0;31mOpção Invalida\033[m')
+            break
+
 
 def atualizarContato():
     global cadastro
