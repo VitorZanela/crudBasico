78493e9c1baf539b078c21e4cd9ce9c2a8751de5
vitorzanela <vitor.mardig@gmail.com>
2025-05-11 01:19:30 -0300

    Ajustado alguns detalhes de layout do codigo

--- modulos/layout/__init__.py
+++ modulos/layout/__init__.py
@@ -1,7 +1,27 @@
+from time import sleep
+
 def linha(tam=40):
     print('-'*tam)
 
 def cabecalho(txt):
     linha()
-    print(txt.center(40))
+    print(F'   \033[1m{txt}\033[m'.center(40))
     linha()
+
+def menu(lista):
+    while True:
+        sleep(1)
+        cabecalho('MENU PRINCIPAL')
+        for chave, valor in enumerate(lista):
+            print(f'{chave+1} - {valor}')
+        linha()
+        opc = leiaInt('Sua Opção: ')
+        return opc
+    
+def leiaInt(txt):
+    try:
+        n = int(input(txt))
+    except:
+        print('ERRO! Opção Invalida!')
+    else:
+        return n
