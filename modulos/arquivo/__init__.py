7e9cd916d21b9a036ef45f9aa1c5d93862b690c9
vitorzanela <vitor.mardig@gmail.com>
2025-05-11 22:41:53 -0300

    Corrigido função <Excluir Contato> e adicionado função  <atualizarContato>
    Falta correção de alguns comandos do menu!

--- modulos/arquivo/__init__.py
+++ modulos/arquivo/__init__.py
@@ -46,23 +46,52 @@ def adicionarCont(arq,nome,tel,email):
 
 def excluirContatos(arq):
     try:
-        a = open(arq, 'r')
+        with open(arq, 'r') as f:
+            a = f.readlines()
     except:
         print('Erro ao ler arquivo')
     else: 
         for i, pessoa in enumerate(a):
-            dado = pessoa.split(';')
-            dado[2] = dado[2].replace('\n','')
+            dado = pessoa.strip().split(';')
             print(f'{i+1} {dado[0]}')
         linha()
         opção = leiaInt('Digite a opção de Exclusão: ')
-        if opção >= 0 and opção < len(dado):
-            del dado[opção]
-            b = open(arq, 'w') #ta excluindo todos os contatos
+        if opção >= 0 and opção < len(a)+1:
+            del a[opção-1]
+            b = open(arq, 'w')
             b.writelines(a)
             b.close()
         else:
             print('Opção invalida')
 
-    finally:
-        a.close()
+def atualizarContato(arq):
+    try:
+        with open(arq, 'r') as f:
+            a = f.readlines()
+    except:
+        print('Erro ao ler arquivo')
+        return
+    for i, pessoa in enumerate(a):
+        dado = pessoa.strip().split(';')
+        print(f'{i+1} {dado[0]}')
+    linha()
+    opção = leiaInt('Digite qual dado quer alterar: ')
+    if opção >= 1 and opção <= len(a):
+        dado = a[opção - 1].strip().split(';')
+        print('\nDados do contato:')
+        for i, campo in enumerate(dado):
+            print(f'{i+1} - {campo}') 
+        linha()
+        campo = leiaInt('Qual campo deseja alterar? ')
+        if campo >= 1 and campo <= len(dado):
+            novo = input('Digite o novo valor: ')
+            dado[campo - 1] = novo
+            a[opção - 1] = ';'.join(dado) + '\n'
+            b = open(arq, 'w')
+            b.writelines(a)
+            b.close()
+            print('Contato atualizado com sucesso!')
+        else:
+            print('Campo invalido')
+    else:
+        print('Opção invalida')
